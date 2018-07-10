9a40cfbdccc8280d633c613dcc91e5d6b330a5ef
Nuno Caro <nuno.pik@gmail.com>
2018-07-10 22:34:39 +0100

    First draft at contextualized scopes

---
+++ ServiceCollectionBuilderTests/src/scope/ContextualizedService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace pt.ncaro.util.dependencyinjection.scope
+{
+
+    //main
+    public interface IMainService
+    {
+        ISlaveService GetSlaveService(SlaveOptions options);
+    }
+    
+    public class MainService : IMainService
+    {
+        private readonly Func<SlaveOptions, ISlaveService> _producer;
+
+        public MainService(Func<SlaveOptions, ISlaveService> producer)
+        {
+            _producer = producer;
+        }
+
+        public ISlaveService GetSlaveService(SlaveOptions options)
+        {
+            return _producer(options);
+        }
+    }
+
+    //main
+    public interface IAnotherService
+    {
+        int GiveFive();
+    }
+
+    public class SlaveOptions
+    {
+        public string Message { get; set; }
+    }
+
+    //depends on main
+    public interface ISlaveService
+    {
+        string GetMessage();
+    }
+    
+    public class Slave : ISlaveService
+    {
+        private readonly SlaveOptions _options;
+        private readonly IAnotherService _service;
+
+        public Slave(SlaveOptions options, IAnotherService service)
+        {
+            _options = options;
+            _service = service;
+        }
+
+        public string GetMessage()
+        {
+            return $"Message is {_options.Message} and service {_service.GiveFive()}";
+        }
+    }
+    
+    //usar o options pattern para cada contexto
+}
---
+++ ServiceCollectionBuilderTests/src/scope/ScopingTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace pt.ncaro.util.dependencyinjection.scope
+{
+    public class ScopingTests
+    {
+
+        [Fact]
+        public void Scopes()
+        {
+            var sc = new ServiceCollection();
+            sc.AddScoped<IScoped, Scoped>();
+            //option one use options pattern to create a new container
+            //auto inject services from parent components
+            //downside is the creation of a full container on each call
+            sc.Configure<MyOptions>(opt => opt.Message = "cenas");
+            var spi = sc.BuildServiceProvider();
+            var scope1 = spi.CreateScope();
+            var scope2 = spi.CreateScope();
+
+            var s1 = spi.GetService<IScoped>();
+            var s2 = spi.GetService<IScoped>();
+            var s3 = scope1.ServiceProvider.GetService<IScoped>();
+            var s4 = scope1.ServiceProvider.GetService<IScoped>();
+            var s5 = scope2.ServiceProvider.GetService<IScoped>();
+            
+            Assert.Same(s1, s2);
+            Assert.NotSame(s1, s3);
+            Assert.Same(s3, s4);
+            Assert.NotSame(s4, s5);
+            Assert.NotSame(s1, s5);
+
+            //option 2 try to use only scoped services on subcomponents
+            //prevents the container recreaction with every contextualized call
+            //does not allow singletons on subcomponents
+        }
+        
+    }
+
+    public interface IScoped {}
+    
+    public class Scoped : IScoped
+    {
+        
+    }
+
+    public class MyOptions
+    {
+        public string Message { get; set; }
+    }
+}
