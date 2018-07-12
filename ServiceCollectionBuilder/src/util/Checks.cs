18f8ba20c28018d40c431e7e7613b1f9828e9cb2
Caro, Nuno (EXT - PT/Amadora) <nuno.caro.ext@coriant.com>
2018-07-12 22:35:19 +0100

    Atempt to create scoped services. Still need to solve multiple context service creation.

---
+++ ServiceCollectionBuilder/src/util/Checks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pt.ncaro.util.dependencyinjection.util
+{
+    public static class Check
+    {
+        private delegate Exception ThrowException();
+        
+        public static void Argument(bool truth)
+        {
+            Validate(truth,() => new ArgumentException());
+        }
+
+        public static void Argument(bool truth, string message, params object[] format)
+        {
+            Validate(truth, () => new ArgumentException(message.Format(format)));
+        }
+
+        public static void State(bool truth, string message, params object[] format)
+        {
+            Validate(truth, delegate { return new InvalidOperationException(message.Format(format));});//just because
+        }
+
+        private static void Validate(bool truth, ThrowException exception)
+        {
+            if (!truth)
+            {
+                throw exception();
+            }
+        }
+
+        private static string Format(this string _this, params object[] arguments)
+        {
+            return string.Format(_this, arguments);
+        }
+    }
+}
--- ServiceCollectionBuilderTests/src/scope/ContextualizedService.cs
+++ ServiceCollectionBuilderTests/src/scope/ContextualizedService.cs
@@ -1,62 +1,262 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using pt.ncaro.util.dependencyinjection.attributes;
+using pt.ncaro.util.dependencyinjection.builder;
+using pt.ncaro.util.dependencyinjection.util;
+using Xunit;
 
 namespace pt.ncaro.util.dependencyinjection.scope
 {
 
-    //main
-    public interface IMainService
+    interface IS1
     {
-        ISlaveService GetSlaveService(SlaveOptions options);
+        int dep1();
     }
-    
-    public class MainService : IMainService
-    {
-        private readonly Func<SlaveOptions, ISlaveService> _producer;
 
-        public MainService(Func<SlaveOptions, ISlaveService> producer)
+    interface IS2
+    {
+        int dep2();
+    }
+
+    interface ID1
+    {
+        int dep3();
+    }
+
+    [ServiceImplementation(typeof(IS1), component:"main")]
+    class S1 : IS1
+    {
+        private readonly Opt _options;
+
+        public S1(IOptions<Opt> options)
         {
-            _producer = producer;
+            _options = options.Value;
         }
 
-        public ISlaveService GetSlaveService(SlaveOptions options)
+        public int dep1()
         {
-            return _producer(options);
+            return 1;
         }
     }
 
-    //main
-    public interface IAnotherService
+    [ServiceImplementation(typeof(IS2), component:"main")]
+    class S2 : IS2
     {
-        int GiveFive();
+        public int dep2()
+        {
+            return 2;
+        }
     }
 
-    public class SlaveOptions
+    [ServiceImplementation(typeof(ID1), component:"slave")]
+    class D1:ID1
+    {
+        public int dep3()
+        {
+            return 3;
+        }
+    }
+
+    class Opt
     {
         public string Message { get; set; }
     }
 
-    //depends on main
-    public interface ISlaveService
+    public class TestClass
     {
-        string GetMessage();
-    }
-    
-    public class Slave : ISlaveService
-    {
-        private readonly SlaveOptions _options;
-        private readonly IAnotherService _service;
 
-        public Slave(SlaveOptions options, IAnotherService service)
+        [Fact]
+        public void CreateDependency()
         {
-            _options = options;
-            _service = service;
+            var main = ServiceCollectionBuilder.Create()
+                .AddCurrentAssembly("main").Build();
+            
+            var slave = ServiceCollectionBuilder.Create()
+                .AddCurrentAssembly("slave").Build();
+
+            
         }
 
-        public string GetMessage()
+        [Fact]
+        public void ConfigureIsNotCalledOnScope()
         {
-            return $"Message is {_options.Message} and service {_service.GiveFive()}";
+            var counter = 0;
+            var spi = new ServiceCollection()
+                .AddScoped<IS1, S1>()
+                .Configure<Opt>(o =>
+                {
+                    counter++;
+                    o.Message = System.DateTime.UtcNow.ToLongTimeString();
+                })
+                .BuildServiceProvider();
+
+            Assert.Equal(0, counter);
+            spi.GetRequiredService<IS1>();
+            Assert.Equal(1, counter);
+            spi.GetRequiredService<IS1>();
+            Assert.Equal(1, counter);
+            spi.CreateScope().ServiceProvider.GetRequiredService<IS1>();
+            Assert.Equal(1, counter);
+
+        }
+
+        [Fact]
+        public void CreateScopedDependency()
+        {
+            var spi = new ServiceCollection()
+                .AddScoped<ScopedService>()
+                .AddScoped<IScopeCtx<Context>, ScopeContext<Context>>().BuildServiceProvider();
+
+            var scope1 = spi.CreateScope();
+            ((ScopeContext<Context>)scope1.ServiceProvider.GetRequiredService<IScopeCtx<Context>>()).SetContext(new Context() {Value = 3});
+            var value1 = scope1.ServiceProvider.GetRequiredService<ScopedService>().GetContextValue();
+            Assert.Equal(3, value1);
+
+            var scope2 = spi.CreateScope();
+            ((ScopeContext<Context>)scope2.ServiceProvider.GetRequiredService<IScopeCtx<Context>>()).SetContext(new Context() {Value = 50});
+            var value2 = scope2.ServiceProvider.GetRequiredService<ScopedService>().GetContextValue();
+            Assert.Equal(50, value2);
+            
+            //using old scopes
+            value1 = scope1.ServiceProvider.GetRequiredService<ScopedService>().GetContextValue();
+            Assert.Equal(3, value1);
+            value2 = scope2.ServiceProvider.GetRequiredService<ScopedService>().GetContextValue();
+            Assert.Equal(50, value2);
+        }
+
+        [ServiceImplementation(typeof(ScopedService), component:"scoped")]
+        class ScopedService
+        {
+            private readonly Context _context;
+
+            public ScopedService(IScopeCtx<Context> context)
+            {
+                _context = context.Context;
+            }
+
+            public int GetContextValue()
+            {
+                return _context.Value;
+            }
+        }
+
+        class Context
+        {
+            public int Value { get; set; }
+
+            public Context(int value = 0)
+            {
+                Value = value;
+            }
+        }
+
+        [Fact]
+        public void TestScopeBuilder()
+        {
+            var sb = new ScopeBuilder()
+                .Assembly(Assembly.GetExecutingAssembly())
+                .Component("scoped")
+                .Context<Context>();
+
+            var spi1 = sb.CreateScope(new Context(123));
+            var value1 = spi1.GetRequiredService<ScopedService>().GetContextValue();
+            Assert.Equal(123, value1);
+            
+            var spi2 = sb.CreateScope(new Context(999));
+            var value2 = spi1.GetRequiredService<ScopedService>().GetContextValue();
+            Assert.Equal(999, value2);
+            
+            var value = spi1.GetRequiredService<ScopedService>().GetContextValue();
+            Assert.Equal(123, value1);
+            value2 = spi1.GetRequiredService<ScopedService>().GetContextValue();
+            Assert.Equal(999, value2);
+        }
+        
+    }
+
+    public class ScopeBuilder
+    {
+        private string _component;
+        private IList<Assembly> _assemblies = new List<Assembly>();
+        private IList<Tuple<Type, object>> _services = new List<Tuple<Type, object>>();
+
+        public ScopeBuilder Component(string component)
+        {
+            _component = component;
+            return this;
+        }
+
+        public ScopeBuilder Assembly(Assembly assembly)
+        {
+            _assemblies.Add(assembly);
+            return this;
+        }
+
+        public ScopeBuilder InheritService<S,I>(I serviceInstance) where S : class where I : S
+        {
+            _services.Add(new Tuple<Type, object>(typeof(S), serviceInstance));
+            return this;
+        }
+
+        public ScopedSpiBuilder<C> Context<C>() where C : class
+        {
+            Check.State(_assemblies.Any(), "Assemblies not provided");
+            
+            var scb = ServiceCollectionBuilder.Create();
+            foreach (var assembly in _assemblies)
+            {
+                scb.AddAssembly(assembly, _component);
+            }
+
+            var sc = scb.Build();
+            foreach (var (type, instance) in _services)
+            {
+                //add scoped does not make sense here.
+                //inherited services act as singletons because their lifecicle should be chained
+                //if service B depends on scoped service A, then A is disposed B should be disposed
+                sc.AddSingleton(type, instance);
+            }
+
+            sc.AddScoped<IScopeCtx<C>, ScopeContext<C>>();
+            return new ScopedSpiBuilder<C>(sc.BuildServiceProvider());
         }
     }
+
+    public class ScopedSpiBuilder<C> where C : class
+    {
+        private readonly IServiceProvider _spi;
+
+        public ScopedSpiBuilder(IServiceProvider spi)
+        {
+            _spi = spi;
+        }
+
+        public IServiceProvider CreateScope(C context)
+        {
+            var newSpi = _spi.CreateScope().ServiceProvider;
+            var contextService = (ScopeContext<C>)newSpi.GetRequiredService<IScopeCtx<C>>();
+            contextService.SetContext(context);
+            return newSpi;
+        }
+    }
+
+    interface IScopeCtx<C>
+    {
+        C Context { get; }
+    }
     
-    //usar o options pattern para cada contexto
+    //Cannot segregate set in different interface because 2 instances would be created
+    internal class ScopeContext<C> : IScopeCtx<C>
+    {
+        public C Context { get; private set; }
+
+        public void SetContext(C context)
+        {
+            Context = context;
+        }
+    }
 }
